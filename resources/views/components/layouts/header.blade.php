03dfd679d59dd35ecde1234af1e9205507b3622e
akrommm <102010542+akrommm@users.noreply.github.com>
2024-02-22 19:35:49 +0700

    logo

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -6,21 +6,19 @@
             <div class="navbar-brand-box">
                 <a href="{{url('Dashboard')}}" class="logo logo-dark">
                     <span class="logo-sm">
-                        <img src="{{ url('/') }}/admin-asset/assets/images/1.png" alt=""  style="width: 100px; height:70px;">
+                        <img src="{{ url('/') }}/admin-asset/assets/images/Logo-sm.png" alt="" style="width: auto; height:35px;">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ url('/') }}/admin-asset/assets/images/1.png" alt="" style="width: 100px; height:70px;">
+                        <img src="{{ url('/') }}/admin-asset/assets/images/1.png" alt="" style="width: auto; height:70px;">
                     </span>
                 </a>
 
                 <a href="index.html" class="logo logo-light">
                     <span class="logo-sm">
-                        <img src="{{ url('/') }}/admin-asset/assets/images/logo-sm-light.png" alt=""
-                            height="22">
+                        <img src="{{ url('/') }}/admin-asset/assets/images/logo-sm-light.png" alt="" height="22">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ url('/') }}/admin-asset/assets/images/logo-light.png" alt=""
-                            height="24">
+                        <img src="{{ url('/') }}/admin-asset/assets/images/logo-light.png" alt="" height="24">
                     </span>
                 </a>
             </div>
@@ -33,8 +31,7 @@
 
 
             <div class="dropdown d-none d-lg-inline-block align-self-center">
-                <button class="btn btn-header waves-effect  dropdown-toggle" type="button" id="createNewDropdown"
-                    data-bs-toggle="dropdown" aria-expanded="false">
+                <button class="btn btn-header waves-effect  dropdown-toggle" type="button" id="createNewDropdown" data-bs-toggle="dropdown" aria-expanded="false">
                     Create New<i class="mdi mdi-chevron-down ms-2"></i>
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="createNewDropdown">
@@ -51,21 +48,17 @@
 
         <div class="d-flex">
             <div class="dropdown d-inline-block d-lg-none ms-2">
-                <button type="button" class="btn header-item noti-icon waves-effect" id="page-header-search-dropdown"
-                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <button type="button" class="btn header-item noti-icon waves-effect" id="page-header-search-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="mdi mdi-magnify"></i>
                 </button>
-                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0"
-                    aria-labelledby="page-header-search-dropdown">
+                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0" aria-labelledby="page-header-search-dropdown">
 
                     <form class="p-3">
                         <div class="form-group m-0">
                             <div class="input-group">
-                                <input type="text" class="form-control" placeholder="Search ..."
-                                    aria-label="Recipient's username">
+                                <input type="text" class="form-control" placeholder="Search ..." aria-label="Recipient's username">
                                 <div class="input-group-append">
-                                    <button class="btn btn-primary" type="submit"><i
-                                            class="mdi mdi-magnify"></i></button>
+                                    <button class="btn btn-primary" type="submit"><i class="mdi mdi-magnify"></i></button>
                                 </div>
                             </div>
                         </div>
@@ -84,14 +77,11 @@
 
             <!-- Notification Dropdown -->
             <div class="dropdown d-inline-block">
-                <button type="button" class="btn header-item noti-icon waves-effect"
-                    id="page-header-notifications-dropdown" data-bs-toggle="dropdown" aria-haspopup="true"
-                    aria-expanded="false">
+                <button type="button" class="btn header-item noti-icon waves-effect" id="page-header-notifications-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="mdi mdi-bell"></i>
                     <span class="badge bg-info rounded-pill">3</span>
                 </button>
-                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0"
-                    aria-labelledby="page-header-notifications-dropdown">
+                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0" aria-labelledby="page-header-notifications-dropdown">
                     <h5 class="p-3 text-dark mb-0">Notifications (37)</h5>
                     <div data-simplebar style="max-height: 230px;">
                         <a href="" class="text-reset notification-item">
@@ -192,30 +182,22 @@
 
             <!-- User -->
             <div class="dropdown d-inline-block">
-                <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
-                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="rounded-circle header-profile-user"
-                        src="{{ url('/') }}/admin-asset/assets/images/users/avatar-4.jpg" alt="Header Avatar">
+                <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img class="rounded-circle header-profile-user" src="{{ url('/') }}/admin-asset/assets/images/users/avatar-4.jpg" alt="Header Avatar">
                 </button>
 
                 <div class="dropdown-menu dropdown-menu-end">
                     <!-- item-->
-                    <a class="dropdown-item" href="#"><i
-                            class="mdi mdi-account-circle font-size-16 align-middle me-2 text-muted"></i>
+                    <a class="dropdown-item" href="#"><i class="mdi mdi-account-circle font-size-16 align-middle me-2 text-muted"></i>
                         <span>Profile</span></a>
-                    <a class="dropdown-item" href="#"><i
-                            class="mdi mdi-wallet font-size-16 align-middle text-muted me-2"></i>
+                    <a class="dropdown-item" href="#"><i class="mdi mdi-wallet font-size-16 align-middle text-muted me-2"></i>
                         <span>My Wallet</span></a>
-                    <a class="dropdown-item d-block" href="#"><span
-                            class="badge bg-success float-end">11</span><i
-                            class="mdi mdi-wrench font-size-16 align-middle text-muted me-2"></i>
+                    <a class="dropdown-item d-block" href="#"><span class="badge bg-success float-end">11</span><i class="mdi mdi-wrench font-size-16 align-middle text-muted me-2"></i>
                         <span>Settings</span></a>
-                    <a class="dropdown-item" href="#"><i
-                            class="mdi mdi-lock-open-outline font-size-16 text-muted align-middle me-2"></i>
+                    <a class="dropdown-item" href="#"><i class="mdi mdi-lock-open-outline font-size-16 text-muted align-middle me-2"></i>
                         <span>Lock screen</span></a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item text-primary" href="#"><i
-                            class="mdi mdi-power font-size-16 align-middle me-2 text-primary"></i>
+                    <a class="dropdown-item text-primary" href="#"><i class="mdi mdi-power font-size-16 align-middle me-2 text-primary"></i>
                         <span>Logout</span></a>
                 </div>
             </div>
